1d910070750a4872a0c1568a99c11cbeb0d2af6d
Andrea Cosentino <ancosen@gmail.com>
2015-09-21 17:10:07 +0200

    Add Example with Multiple topic

---
+++ kafka-quickstart-producer-multiple-topics/src/main/java/com/github/oscerd/ProducerMultipleTopic.java
@@ -0,0 +1,28 @@
+package com.github.oscerd;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class ProducerMultipleTopic {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", StringSerializer.class.getName());
+        props.put("value.serializer", StringSerializer.class.getName());
+        
+        KafkaProducer<String, String> prod = new KafkaProducer<String, String>(props);
+
+        ProducerRecord<String, String> data1 = new ProducerRecord<String, String>("test", "test");
+        ProducerRecord<String, String> data2 = new ProducerRecord<String, String>("test1", "test1");
+
+        prod.send(data1);
+        prod.send(data2);
+
+        prod.close();
+    }
+}
